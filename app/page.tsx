7320d696bf097db8a9437e913a25c3b91be5d051
Noblixzy <121813330+adityaREDFLAG@users.noreply.github.com>
2024-10-05 21:50:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-'use client';import { useEffect, useState } from 'react';
+'use client';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { FaHeart, FaShareAlt, FaHome, FaStar } from 'react-icons/fa';
 
