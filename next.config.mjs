5a3572bdde1c618f3d6a5f73ed4ca2dba74852fb
Noblixzy <121813330+adityaREDFLAG@users.noreply.github.com>
2024-10-06 12:07:39 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.mjs
+export const nextConfig = {
   output: 'export', // Enables Next.js to export the app as static files
   trailingSlash: true, // Optional but helps in static hosting environments
   images: {
     unoptimized: true, // Disable Next.js image optimization for static exports
-  },
-};
+  }
+}
 
-export default nextConfig;
+export default nextConfig; // Make sure to export it as default as well
