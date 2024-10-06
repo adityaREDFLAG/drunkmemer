8a3bdd17f73a194e907076ed2d55cb4d84d23918
Noblixzy <121813330+adityaREDFLAG@users.noreply.github.com>
2024-10-06 12:15:38 +0530

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,10 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Enables Next.js to export the app as static files
+  trailingSlash: true, // Optional but helps in static hosting environments
+  images: {
+    unoptimized: true, // Disable Next.js image optimization for static exports
   },
 };
 
-export default config;
+export default nextConfig;
