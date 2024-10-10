015a2437dbd4ab0f1a49d566a0bc84d99455655f
Noblixzy <121813330+adityaREDFLAG@users.noreply.github.com>
2024-10-10 09:42:27 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./global.css";
-
-/* Load custom fonts with appropriate weights */
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: ["100", "900"], // Updated to use array format for weight
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: ["100", "900"],
-});
 
 /* Metadata for the site */
 export const metadata: Metadata = {
